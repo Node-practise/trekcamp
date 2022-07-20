fdef7c599c9a4318480a5902491f040bbec736e4
RANJAN <spak@RANJANs-MacBook-Pro.local>
2022-07-20 14:00:28 +0530

    add procfile

--- app.js
+++ app.js
@@ -37,9 +37,9 @@ db.once('open', () => {
 })
 // Connection to database END
 
-
+const PORT = process.env.port || 3000
 const app = express()
-app.listen(3000, () => console.log('Listening to port 3000'))
+app.listen(PORT, () => console.log('Listening to port 3000'))
 
 
 app.engine('ejs', ejsMate)
